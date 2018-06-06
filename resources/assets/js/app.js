1e00c18c353878a8f9ef28282e36ef0ab265232c
Angel González <ing.angel.gonzalez@gmail.com>
2018-06-06 20:45:10 +0200

    initial commit

---
+++ resources/assets/js/app.js
@@ -0,0 +1,17 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.app = app = angular.module('app',[]);
+
+app.component('test',{
+    template:`<p>{{$ctrl.text}}</p>`,
+    controller:[function testComponent() {
+        this.text = 'Contenido del componente'
+    }]
+});
